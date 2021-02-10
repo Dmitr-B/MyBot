9ddeb7f4e0496ee788ed19e3b0b83b2c94d34435
Dmytro Butko <dmitri.butko@ukr.net>
2021-02-10 22:57:53 +0200

    Adding Update.java and update BotController.java

---
+++ src/main/java/com/butko/springcourse/botapp/Update.java
@@ -0,0 +1,35 @@
+package com.butko.springcourse.botapp;
+
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.logging.log4j.message.Message;
+
+public class Update {
+    @JsonProperty("update_id")
+    private Integer update_id;
+    //private Message message;
+
+    @JsonCreator
+    public Update(Integer update_id/*, Message message*/) {
+        this.update_id = update_id;
+        //this.message = message;
+    }
+
+
+    public Integer getUpdate_id() {
+        return update_id;
+    }
+
+    public void setUpdate_id(Integer update_id) {
+        this.update_id = update_id;
+    }
+
+    /*public Message getMessage() {
+        return message;
+    }
+
+    public void setMessage(Message message) {
+        this.message = message;
+    }*/
+}
--- src/main/java/com/butko/springcourse/botapp/controllers/BotController.java
+++ src/main/java/com/butko/springcourse/botapp/controllers/BotController.java
@@ -1,10 +1,13 @@
 package com.butko.springcourse.botapp.controllers;
 
+import com.butko.springcourse.botapp.Update;
 import org.apache.coyote.Response;
+import org.apache.logging.log4j.message.Message;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,7 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BotController {
 
     @PostMapping()
-    public ResponseEntity<String> display(){
+    public ResponseEntity<String> display(@RequestBody()Update update){
+        System.out.println(update.getUpdate_id());
+        //System.out.println(update.getMessage());
+        System.out.println(ResponseEntity.ok().build());
         return ResponseEntity.ok().build();
     }
 }
