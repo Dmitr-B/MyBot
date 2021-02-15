c9e82728479d0cf81642531aa09913341c368148
Dmytro Butko <dmitri.butko@ukr.net>
2021-02-15 23:45:28 +0200

    Added Optional

--- src/main/java/com/butko/springcourse/botapp/repository/ChatRepository.java
+++ src/main/java/com/butko/springcourse/botapp/repository/ChatRepository.java
@@ -4,6 +4,10 @@ import com.butko.springcourse.botapp.repository.domain.Chat;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ChatRepository extends CrudRepository<Chat, Long> {
+    Optional<Chat> findByChatId(long chatId);
 }
--- src/main/java/com/butko/springcourse/botapp/service/ChatService.java
+++ src/main/java/com/butko/springcourse/botapp/service/ChatService.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 @Log4j2
@@ -20,7 +22,9 @@ public class ChatService {
         chat.setFirstName(update.getMessage().getChat().getFirstName());
         chat.setLastName(update.getMessage().getChat().getLastName());
         chat.setText(update.getMessage().getText());
-        log.info("Save data to DB: " + chat);
-        chatRepository.save(chat);
+        if (!chatRepository.findByChatId(chat.getChatId()).isPresent()) {
+            log.info("Save data to DB: " + chat);
+            chatRepository.save(chat);
+        } else log.info("ChatId is already defined");
     }
 }
